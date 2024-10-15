b6b576965f069489a41f78bb38a198d22388979f
Ethan-C11 <ethan.collignon.1102@gmail.com>
2024-10-15 16:42:53 +0200

    Phrase de victoire (ou défaite)

--- lib/casino_index.dart
+++ lib/casino_index.dart
@@ -1,8 +1,22 @@
 import 'dart:math';
+import 'enum_jackpot.dart';
 
 class CasinoIndex {
-
   static List<int> playCasino() {
-     return [Random().nextInt(8), Random().nextInt(8), Random().nextInt(8)];
+    return [Random().nextInt(7)+1, Random().nextInt(7)+1, Random().nextInt(7)+1];
+  }
+
+  static jackpotType isJackpot(List<int> casinoResult) {
+    int slot1 = casinoResult[0];
+    int slot2 = casinoResult[1];
+    int slot3 = casinoResult[2];
+
+    if (slot1 == 7 && slot2 == 7 && slot3 == 7) {
+      return jackpotType.tripleSeven;
+    } else if(slot1 == slot2 && slot1 == slot3 && slot3 == slot2)  {
+      return jackpotType.jackpot; // pas de jackpot
+    } else {
+      return jackpotType.notJackpot;
+    }
   }
 }
--- lib/casino_widget.dart
+++ lib/casino_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import './casino_index.dart';
-
+import 'enum_jackpot.dart';
 
 class CasinoWidget extends StatefulWidget {
   const CasinoWidget({super.key, required this.title});
+
   final String title;
 
   @override
@@ -11,7 +12,8 @@ class CasinoWidget extends StatefulWidget {
 }
 
 class _CasinoWidgetState extends State<CasinoWidget> {
-  List<int> _casinoResult = [0,0,0];
+  List<int> _casinoResult = [0, 0, 0];
+  jackpotType? _isJackpot = null;
 
   void setCasinoResult(int slot1, int slot2, int slot3) {
     setState(() {
@@ -19,21 +21,46 @@ class _CasinoWidgetState extends State<CasinoWidget> {
     });
   }
 
+  void setIsJackpot(jackpotType type) {
+    setState(() {
+      _isJackpot = type;
+    });
+  }
+
   void _onButtonPressed() {
     List<int> casinoResult = CasinoIndex.playCasino();
     setCasinoResult(casinoResult[0], casinoResult[1], casinoResult[2]);
+    setIsJackpot(CasinoIndex.isJackpot(casinoResult));
   }
 
   @override
   Widget build(BuildContext context) {
 
+    TextStyle winningSentenceStyle() {
+      return TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+    }
+
     Widget cleanBuild() {
-      return  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_casinoResult.toString())
-          ],
-        );
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(_casinoResult.toString()),
+          Builder(
+            builder: (context) {
+              switch (_isJackpot) {
+                case jackpotType.notJackpot:
+                  return Text('You lost, try again !', style: winningSentenceStyle());
+                case jackpotType.jackpot:
+                  return Text('Jackpot', style: winningSentenceStyle());
+                case jackpotType.tripleSeven:
+                  return Text('SUPER JACKPOT !!!', style: winningSentenceStyle());
+                default:
+                  return const Text('');
+              }
+            },
+          )
+        ],
+      );
     }
 
     return Scaffold(
@@ -41,13 +68,11 @@ class _CasinoWidgetState extends State<CasinoWidget> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center( child: cleanBuild()
-      ),
+      body: Center(child: cleanBuild()),
       floatingActionButton: FloatingActionButton(
-        onPressed: _onButtonPressed,
-        tooltip: 'Play',
-        child: const Icon(Icons.loop)
-      ),
+          onPressed: _onButtonPressed,
+          tooltip: 'Play',
+          child: const Icon(Icons.loop)),
     );
   }
 }
